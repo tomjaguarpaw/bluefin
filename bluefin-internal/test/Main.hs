113be06e07e8cf922094fade9baf0adf140c6b2c
Tom Ellis <tom-git@jaguarpaw.co.uk>
2024-02-03 13:50:07 +0000

    First test

--- bluefin-internal/test/Main.hs
+++ bluefin-internal/test/Main.hs
@@ -1,4 +1,27 @@
 module Main (main) where
 
+import Bluefin.Internal
+import Control.Monad (when)
+import Data.Foldable (for_)
+import System.Exit (ExitCode (ExitFailure), exitWith)
+import Prelude hiding (break)
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented."
+main =
+  if oddsUntilFirstGreaterThan5 == [1, 3, 5, 7]
+    then pure ()
+    else exitWith (ExitFailure 1)
+
+oddsUntilFirstGreaterThan5 :: [Int]
+oddsUntilFirstGreaterThan5 =
+  fst $
+    runEff $
+      yieldToList $ \y -> do
+        withJump $ \break -> do
+          for_ [1 .. 10] $ \i -> do
+            withJump $ \continue -> do
+              when (i `mod` 2 == 0) $
+                jumpTo continue
+              yield y i
+              when (i > 5) $
+                jumpTo break
