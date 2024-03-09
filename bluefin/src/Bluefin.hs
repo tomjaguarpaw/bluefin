a90cbc511ee96e4092fe3834f9d2d75a8d96b364
Tom Ellis <tom-git@jaguarpaw.co.uk>
2024-03-09 07:18:35 +0000

    Fix links
    Fixes https://github.com/tomjaguarpaw/bluefin/issues/1

--- bluefin/src/Bluefin.hs
+++ bluefin/src/Bluefin.hs
@@ -124,8 +124,8 @@ module Bluefin
     --
     -- It would be great to share code between the two projects!  But
     -- there are two Bluefin features that I don't know to implement
-    -- in terms of effectful: 'Bluefin.Bluefin.Coroutine's and
-    -- 'Bluefin.Bluefin.Compound' effects.
+    -- in terms of effectful: 'Bluefin.Coroutine.Coroutine's and
+    -- 'Bluefin.Compound.Compound' effects.
 
     -- * Implementation
 
