d859edaace93fa15d2886e95d46db4bbf423d451
Tom Ellis <tom-git@jaguarpaw.co.uk>
2024-03-23 13:04:12 +0000

    Document Stream

--- bluefin/src/Bluefin/Stream.hs
+++ bluefin/src/Bluefin/Stream.hs
@@ -1,5 +1,12 @@
 module Bluefin.Stream
-  ( -- * Handle
+  ( -- | 'Stream' allows you to yield values during the execution of a
+    -- Bluefin operation.  It provides similar functionality to
+    -- Python's @yield@.  The handler of the 'Stream' will either
+    -- handle each element as soon as it is yielded (for example
+    -- 'forEach') or gather all yielded elements int o a list (for
+    -- example 'yieldToList').
+
+    -- * Handle
     Stream,
     -- * Handlers
     forEach,
