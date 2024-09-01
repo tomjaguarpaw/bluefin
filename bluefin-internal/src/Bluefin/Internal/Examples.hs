f5ff2fed09bb362027123499492e7aa811d69e93
Tom Ellis <tom-git@jaguarpaw.co.uk>
2024-09-01 15:57:57 +0100

    Tuple

--- bluefin-internal/src/Bluefin/Internal/Examples.hs
+++ bluefin-internal/src/Bluefin/Internal/Examples.hs
@@ -712,9 +712,10 @@ linearlyExample = runEff $ \io ->
             (\() y -> for_ ['A' .. 'F'] $ \i -> yield y i)
             ( \l1 ->
                 linearly
-                  (\() y -> for_ [1 .. 3] $ \i -> yield y i)
-                  ( \l2 ->
-                      (alternate out l1 l2)
+                  (\() y -> for_ [1 :: Int .. 3] $ \i -> yield y i)
+                  ( \l2 -> L.do
+                      (d1, d2) <- alternate out l1 l2
+                      L.pure (((), d1), d2)
                   )
             )
     )
@@ -725,12 +726,12 @@ alternate ::
   Stream String e3 ->
   Linearly () a1 () e1 %1 ->
   Linearly () a2 () e2 %1 ->
-  LEff es (((), You'reDone e1), You'reDone e2)
+  LEff es (You'reDone e1, You'reDone e2)
 alternate y l1 l2 =
   yieldLinearly l1 () L.>>= \case
     Right (Ur r, d1) -> L.do
       liftLEff (yield y ("done: " <> show r))
-      yieldAll y (\d2 -> (((), d1), d2)) l2
+      yieldAll y (\d2 -> (d1, d2)) l2
     Left (Ur s, l1') -> L.do
       liftLEff (yield y ("got: " <> show s))
       alternate1 y l2 l1'
@@ -740,12 +741,12 @@ alternate1 ::
   Stream String e3 ->
   Linearly () a1 () e1 %1 ->
   Linearly () a2 () e2 %1 ->
-  LEff es (((), You'reDone e2), You'reDone e1)
+  LEff es (You'reDone e2, You'reDone e1)
 alternate1 y l1 l2 =
   yieldLinearly l1 () L.>>= \case
     Right (Ur r, d1) -> L.do
       liftLEff (yield y ("done: " <> show r))
-      yieldAll y (\d2 -> (((), d2), d1)) l2
+      yieldAll y (\d2 -> (d2, d1)) l2
     Left (Ur s, l1') -> L.do
       liftLEff (yield y ("got: " <> show s))
       alternate y l2 l1'
