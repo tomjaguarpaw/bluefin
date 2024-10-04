855130a07402a380c6bd9a1e8ad016bfb97a7973
Tom Ellis <tom-git@jaguarpaw.co.uk>
2024-10-04 13:23:20 +0100

    Explain that Pipes is just an example

--- bluefin/src/Bluefin/Pipes.hs
+++ bluefin/src/Bluefin/Pipes.hs
@@ -1,4 +1,15 @@
 -- | Reimplementation of the @pipes@ (@Pipes@) ecosystem in Bluefin.
+-- It primarily serves as an example of what you can do with Bluefin
+-- and you probably won't want to use it directly.  Instead you are
+-- recommended to use
+--
+-- * 'Bluefin.Stream', 'Bluefin.Stream.yield'
+-- * 'Bluefin.Consume', 'Bluefin.Consume.await'
+-- * 'Bluefin.Stream.consumeStream'
+-- * For advanced cases only, 'Bluefin.Coroutine',
+--   'Bluefin.Coroutine.yieldCoroutine' and
+--   'Bluefin.Coroutine.connectCoroutines'
+--
 -- See also "Bluefin.Pipes.Prelude".
 module Bluefin.Pipes
   ( -- * The Proxy handle
--- bluefin/src/Bluefin/Pipes/Prelude.hs
+++ bluefin/src/Bluefin/Pipes/Prelude.hs
@@ -1,5 +1,16 @@
 -- | Reimplementation of the @pipes@ prelude (@Pipes.Prelude@) in
--- Bluefin.  See also "Bluefin.Pipes".
+-- Bluefin.  It primarily serves as an example of what you can do with
+-- Bluefin and you probably won't want to use it directly.  Instead
+-- you are recommended to use
+--
+-- * 'Bluefin.Stream', 'Bluefin.Stream.yield'
+-- * 'Bluefin.Consume', 'Bluefin.Consume.await'
+-- * 'Bluefin.Stream.consumeStream'
+-- * For advanced cases only, 'Bluefin.Coroutine',
+--   'Bluefin.Coroutine.yieldCoroutine' and
+--   'Bluefin.Coroutine.connectCoroutines'
+--
+-- See also "Bluefin.Pipes".
 --
 -- @
 -- >>> 'Bluefin.Eff.runEff' $ \\io -> 'runEffect' $ do
