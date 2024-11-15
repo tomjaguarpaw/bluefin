be7fb8b69213dd9294bfb3ff15a5844c9f9afb6b
Tom Ellis <tom-git@jaguarpaw.co.uk>
2024-11-15 16:53:53 +0000

    Fix typo

--- bluefin-internal/src/Bluefin/Internal.hs
+++ bluefin-internal/src/Bluefin/Internal.hs
@@ -316,7 +316,7 @@ type Consume a = Coroutine () a
 -- @
 --
 -- To define @mapHandle@ for @Application@ you should apply
--- @mapHandle@ to all the fields that are themeselevs handles and
+-- @mapHandle@ to all the fields that are themeselves handles and
 -- apply @useImpl@ to all the fields that are dynamic effects:
 --
 -- @
