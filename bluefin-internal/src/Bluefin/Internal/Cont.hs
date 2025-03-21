26ae34f4a19a79b18f14976fda576a0b38430618
jeukshi <orlowd+gh@gmail.com>
2025-03-21 23:33:00 +0100

    Run in `Eff`.

--- bluefin-internal/src/Bluefin/Internal/Cont.hs
+++ bluefin-internal/src/Bluefin/Internal/Cont.hs
@@ -5,24 +5,24 @@ module Bluefin.Internal.Cont where
 import Bluefin.Internal (Eff, Effects, useImplIn, (:&), (:>))
 import Unsafe.Coerce (unsafeCoerce)
 
-newtype EffCont r m a = MkEffCont {unEffCont :: (a -> m r) -> m r}
+newtype EffCont es a = MkEffCont {unEffCont :: forall r. (a -> Eff es r) -> Eff es r}
 
-instance Functor (EffCont r m) where
+instance Functor (EffCont es) where
   fmap f m = MkEffCont $ \c -> unEffCont m (c . f)
 
-instance Applicative (EffCont r m) where
+instance Applicative (EffCont es) where
   pure x = MkEffCont ($ x)
   f <*> v = MkEffCont $ \c -> unEffCont f $ \g -> unEffCont v (c . g)
   m *> k = m >> k
 
-instance Monad (EffCont r m) where
+instance Monad (EffCont es) where
   return = pure
   m >>= k = MkEffCont $ \c -> unEffCont m (\x -> unEffCont (k x) c)
 
 data Cont (e :: Effects) = UnsafeMkCont
 
 runCont ::
-  (forall e. Cont e -> EffCont a (Eff (e :& es)) a) ->
+  (forall e. Cont e -> EffCont (e :& es) a) ->
   Eff es a
 runCont f = unsafeRunCont $ \cont -> do
   unEffCont (f cont) return
@@ -36,17 +36,17 @@ liftEff ::
   (contE :> es) =>
   Cont contE ->
   Eff es a ->
-  EffCont r (Eff es) a
+  EffCont es a
 liftEff _ f = MkEffCont $ \effCont -> f >>= effCont
 
 new ::
   Cont contE ->
   ((forall (e :: Effects). f e -> Eff (e :& es) r) -> Eff es r) ->
-  EffCont r (Eff (contE :& es)) (f contE)
+  EffCont (contE :& es) (f contE)
 new f = MkEffCont . forceEffTag f
   where
     forceEffTag ::
       Cont contE ->
-      ((forall e. f e -> Eff (e :& es) r) -> Eff es r) ->
-      ((f contE -> Eff (contE :& es) r) -> Eff (contE :& es) r)
+      ((forall e. f e -> Eff (e :& es) a) -> Eff es a) ->
+      (forall r. (f contE -> Eff (contE :& es) r) -> Eff (contE :& es) r)
     forceEffTag _ = unsafeCoerce
