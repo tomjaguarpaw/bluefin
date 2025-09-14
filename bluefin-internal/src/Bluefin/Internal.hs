a96e5676788cb4017113b3b1743dcedf0a880ddf
Tom Ellis <tom-git@jaguarpaw.co.uk>
2025-09-14 15:33:00 +0100

    Force signatures to wrap

--- bluefin-internal/src/Bluefin/Internal.hs
+++ bluefin-internal/src/Bluefin/Internal.hs
@@ -284,11 +284,16 @@ inContext :: (e2 :> e1) => Eff (e1 :& e2) r -> Eff e1 r
 inContext = weakenEff (subsume1 has)
 
 -- | Used to define dynamic effects.
-makeOp :: Eff (e :& e) r -> Eff e r
+makeOp :: Eff (e :& e) r ->
+   -- | ͘
+    Eff e r
 makeOp = useImplIn0
 
 -- | Used to define dynamic effects.
-useImpl :: (e :> es) => Eff e r -> Eff es r
+useImpl ::
+  (e :> es) => Eff e r ->
+  -- | ͘
+  Eff es r
 useImpl = weakenEff has
 
 -- | Like 'useImpl'
