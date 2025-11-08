d5240610520ab46dc15016ce805d494fea7dfd11
Tom Ellis <tom-git@jaguarpaw.co.uk>
2025-11-08 16:59:12 +0000

    Add module Haddock for Reader and HandleReader

--- bluefin/src/Bluefin/HandleReader.hs
+++ bluefin/src/Bluefin/HandleReader.hs
@@ -1,3 +1,15 @@
+-- | 'HandleReader' is like t'Bluefin.Reader.Reader', generalized to
+-- work for arbitrary t'Bluefin.Compound.Handle's.  'localHandle'
+-- locally overrides the value of a handle in a well-scoped way.  The
+-- original handle will be restored when you exit the @localHandle@
+-- block regardless of whether the exit was normal or via an
+-- exception.
+--
+-- @HandleReader@ supports functionality similiar to @effectful@'s
+-- 'Effectful.Dispatch.Dynamic.interpose' and @polysemy@'s
+-- 'Polysemy.intercept', that is, locally augmenting an effect with
+-- new behaviors.
+
 module Bluefin.HandleReader
   (-- * Handle
    HandleReader,
--- bluefin/src/Bluefin/Reader.hs
+++ bluefin/src/Bluefin/Reader.hs
@@ -1,6 +1,10 @@
 module Bluefin.Reader
   ( -- | 'Reader' is Bluefin's version of the
-    -- "Control.Monad.Trans.Reader" monad.
+    -- "Control.Monad.Trans.Reader" monad.  'local' allows you to
+    -- locally override the value in the @Reader@ handle in a
+    -- well-scoped way.  The original value will be restored when you
+    -- exit the @local@ block regardless of whether the exit was
+    -- normal or via an exception .
 
     -- * Handle
     Reader,
