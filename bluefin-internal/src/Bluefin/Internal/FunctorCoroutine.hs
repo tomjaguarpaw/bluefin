362e893af1300a676e3fa4adf2cc5b793b975f9f
Tom Ellis <tom-git@jaguarpaw.co.uk>
2025-12-13 17:47:12 +0000

    WIP

--- bluefin-internal/src/Bluefin/Internal/FunctorCoroutine.hs
+++ bluefin-internal/src/Bluefin/Internal/FunctorCoroutine.hs
@@ -24,8 +24,9 @@ instance Handle (Send f) where
 send ::
   (e1 :> es) =>
   Send f e1 ->
+  -- | Handle this operation using the effect handler currently in
+  -- scope for the @Send f@ handle.
   f (Eff es) r ->
-  -- | ͘
   Eff es r
 send (MkSend g) = useImplIn g
 
@@ -36,13 +37,14 @@ type EffectHandler f es =
   Eff (e :& es) r1
 
 interpret ::
+  -- | Implementation of effect handler for @Send f@
   EffectHandler f es ->
+  -- | Within this block, @send@ has the implementation given above.
   (forall e. Send f e -> Eff (e :& es) r) ->
-  -- | ͘
   Eff es r
 interpret g k = useImplIn k (MkSend g)
 
-type Flip :: ((Type -> Type) -> k -> Type) -> k -> Effects -> Type
+type Flip :: ((Type -> Type) -> Type -> Type) -> Type -> Effects -> Type
 newtype Flip f a e = MkFlip {unFlip :: f (Eff e) a}
 
 mapFlip ::
@@ -57,19 +59,22 @@ handleFlip ::
   HandleD (Flip f r)
 handleFlip f = handleMapHandle (mapFlip f)
 
+-- | Version of @send@ for use when pattern matching in @interpose@
 passthrough ::
   (Handle (Flip f r), e1 :> es, e2 :> es) =>
   Send f e1 ->
   f (Eff e2) r ->
-  -- | ͘
   Eff es r
 passthrough fc = send fc . unFlip . mapHandle . MkFlip
 
 interpose ::
   (e1 :> es) =>
+  -- | Reimplementation of effect handler for @Send f@ in terms of the
+  -- the original effect handler, which is passed as the argument
   (Send f es -> EffectHandler f es) ->
+  -- | Original effect handler
   HandleReader (Send f) e1 ->
+  -- | Within this block, @send@ has the implementation given above.
   Eff es r ->
-  -- | ͘
   Eff es r
 interpose h hr = localHandle hr (\fcOrig -> MkSend (h fcOrig))
