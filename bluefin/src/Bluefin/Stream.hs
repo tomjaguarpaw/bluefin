bb701c6e7d8f3affba28047801d47fb1005548b3
Tom Ellis <tom-git@jaguarpaw.co.uk>
2026-04-25 10:14:07 +0100

    (comment) Fix typo

--- bluefin/src/Bluefin/Stream.hs
+++ bluefin/src/Bluefin/Stream.hs
@@ -3,7 +3,7 @@ module Bluefin.Stream
     -- Bluefin operation.  It provides similar functionality to
     -- Python's @yield@.  The handler of the 'Stream' will either
     -- handle each element as soon as it is yielded (for example
-    -- 'forEach') or gather all yielded elements int o a list (for
+    -- 'forEach') or gather all yielded elements into a list (for
     -- example 'yieldToList').
     --
     -- For information about prompt finalization/resource safety when
