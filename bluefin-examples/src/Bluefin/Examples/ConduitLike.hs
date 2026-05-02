25691d313d9ddcb18fcca9a7d45cab6ffc33bb83
Tom Ellis <tom-git@jaguarpaw.co.uk>
2026-05-02 15:18:50 +0100

    fixup

--- bluefin-examples/src/Bluefin/Examples/ConduitLike.hs
+++ bluefin-examples/src/Bluefin/Examples/ConduitLike.hs
@@ -1,25 +1,62 @@
-{- cabal:
-build-depends: base, text, directory, conduit
--}
-import Conduit
-import qualified Data.Conduit.Combinators as CC
+module Bluefin.Examples.ConduitLike where
 
-main :: IO ()
-main =
-  runConduitRes $
-       CC.enumFromTo (1 :: Int) 4
-    .| awaitForever (\x ->
-       bracketP (pure ()) (\_ -> putStrLn "close") 
-         (\_ -> CC.enumFromTo (1 :: Int) x) 
-       .| takeRes 3)
-    .| CC.mapM_ (liftIO . print)
-    .| sinkNull
+import Bluefin.Compound
+import Bluefin.Consume
+import Bluefin.Eff
+import Bluefin.IO
+import Bluefin.Stream
+import Control.Monad
 
-takeRes :: MonadResource m => Int -> ConduitT a a m ()
-takeRes 0 = liftResourceT $ do
-  is <- getInternalState
-  closeInternalState is
-  liftIO $ writeIORef is $ ReleaseMap maxBound (minBound + 1) mempty
-takeRes n = await >>= \case
-  Nothing -> mempty
-  Just x -> yield x *> takeRes (n - 1)
+infixr 9 `connect`
+
+connect ::
+  ( forall e.
+    consume ->
+    Stream b e ->
+    Eff (e :& es) r
+  ) ->
+  ( forall e.
+    Consume b e ->
+    stream ->
+    Eff (e :& es) r
+  ) ->
+  consume ->
+  stream ->
+  Eff es r
+connect f1 f2 c s = streamConsume (\s' -> f1 c s') (\c' -> f2 c' s)
+
+asConsume ::
+  (e1 <: es) =>
+  ((forall e. Stream a e -> Eff (e :& es) ()) -> stream -> Eff es ()) ->
+  Consume a e1 ->
+  stream ->
+  Eff es ()
+asConsume k c s = k (\y -> forever (await c >>= yield y)) s
+
+foo :: IOE e -> consume -> stream -> Eff e ()
+foo io =
+  (\_ -> inFoldable ['A' .. 'Z'])
+    `connect` asConsume (enumerateFrom 5)
+    `connect` takeConsume 5
+    `connect` takeConsume 4
+    `connect` takeConsume 3
+    `connect` printC io
+
+printC ::
+  (e1 <: es, e2 <: es) =>
+  (Show a) =>
+  IOE e1 ->
+  Consume a e2 ->
+  stream ->
+  Eff es r
+printC io c _ = forever ((effIO io . print) =<< await c)
+
+run ::  (() -> () -> Eff es r) ->  Eff es r
+run k = k () ()
+
+-- > example
+-- (5,'A')
+-- (6,'B')
+-- (7,'C')
+example :: IO ()
+example = runEff_ $ \io -> run (\c s -> foo (mapHandle io) c s)
