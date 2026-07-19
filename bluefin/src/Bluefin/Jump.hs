d4a738480e9e9faecafc48e4ff83aece9235ad96
Tom Ellis <tom-git@jaguarpaw.co.uk>
2026-07-19 18:46:32 +0100

    (haddock) Fix link target: Bluefin.Capability.JumpTo

--- bluefin/src/Bluefin/Jump.hs
+++ bluefin/src/Bluefin/Jump.hs
@@ -1,5 +1,5 @@
 -- | This is an old interface and will be deprecated in the
--- future. You are encouraged to use "Bluefin.Capability.Jump" instead.
+-- future. You are encouraged to use "Bluefin.Capability.JumpTo" instead.
 module Bluefin.Jump
   ( -- | 'Jump' allows you to jump back to a previously-set location.
     -- A "jump" is equivalent to an untyped early return, or more
