e15fdd58f60639e2e8b3a107d085c36c39301ee9
Tom Ellis <tom-git@jaguarpaw.co.uk>
2026-07-19 18:32:04 +0100

    (feature) Add more Pipes functionality to Bluefin.Examples.Pipes

--- bluefin-examples/src/Bluefin/Examples/Pipes.hs
+++ bluefin-examples/src/Bluefin/Examples/Pipes.hs
@@ -21,18 +21,22 @@ module Bluefin.Examples.Pipes
     Producer,
     yield,
     for,
+    stdinLn,
     (~>),
     (<~),
 
     -- ** Consumers
     Consumer,
     await,
+    stdoutLn,
+    print,
     (>~),
     (~<),
 
     -- ** Pipes
     Pipe,
     cat,
+    takeWhile',
     (>->),
     (<-<),
 
@@ -44,3 +48,51 @@ where
 
 -- Eventually we'll copy the actual implementation here
 import Bluefin.Pipes
+import Bluefin.Eff
+import Bluefin.IO
+import Control.Monad
+import Bluefin.Capability.ReturnEarly
+import Prelude hiding (break, print, takeWhile)
+import Prelude qualified
+
+stdinLn ::
+  (e1 <: es, e2 <: es) =>
+  IOE e1 ->
+  Producer String e2 ->
+  -- | ͘
+  Eff es r
+stdinLn io c = forever $ do
+  line <- effIO io getLine
+  yield c line
+
+stdoutLn ::
+  (e1 <: es, e2 <: es) =>
+  IOE e1 ->
+  Consumer String e2 ->
+  -- | ͘
+  Eff es r
+stdoutLn io c = forever $ do
+  line <- await c
+  effIO io (putStrLn line)
+
+takeWhile' ::
+  (e <: es) =>
+  (r -> Bool) ->
+  Pipe r r e ->
+  -- | ͘
+  Eff es r
+takeWhile' predicate p = withReturnEarly $ \early -> forever $ do
+  a <- await p
+  if predicate a
+    then yield p a
+    else returnEarly early a
+
+print ::
+  (e2 <: es, e1 <: es, Show a) =>
+  IOE e1 ->
+  Consumer a e2 ->
+  -- | ͘
+  Eff es r
+print io p = forever $ do
+  a <- await p
+  effIO io (Prelude.print a)
