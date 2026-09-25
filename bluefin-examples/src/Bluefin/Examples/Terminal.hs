5588850a04830bd61ef9595edbe2823b5be8b27b
Tom Ellis <tom-git@jaguarpaw.co.uk>
2026-09-25 16:48:55 +0100

    (example) Add dynamic Terminal attenuation

--- bluefin-examples/src/Bluefin/Examples/Terminal.hs
+++ bluefin-examples/src/Bluefin/Examples/Terminal.hs
@@ -10,6 +10,8 @@ import Bluefin.Compound
     OneWayCoercibleHandle (..),
     mapHandle,
     oneWayCoercibleNewtypeHandle,
+    oneWayCoercibleTrustMe,
+    useImpl,
     useImplIn,
   )
 import Bluefin.Eff (Eff, runEff, type (:&), type (<:))
@@ -45,3 +47,50 @@ staticTerminalAction terminal = do
   putStrLn terminal "What is your name?"
   name <- getLine terminal
   putStrLn terminal ("Hello, " <> name)
+
+-- | A dynamic terminal capability. Its implementation is supplied as a
+-- product of effectful operations rather than fixed by the capability type.
+data DynamicTerminal e = MkDynamicTerminal
+  { dynamicGetLineImpl :: Eff e String,
+    dynamicPutStrLnImpl :: String -> Eff e ()
+  }
+  deriving (Handle) via OneWayCoercibleHandle DynamicTerminal
+
+instance
+  (e <: es) =>
+  OneWayCoercible (DynamicTerminal e) (DynamicTerminal es)
+  where
+  oneWayCoercibleImpl = oneWayCoercibleTrustMe $ \terminal ->
+    MkDynamicTerminal
+      { dynamicGetLineImpl = useImpl (dynamicGetLineImpl terminal),
+        dynamicPutStrLnImpl = useImpl . dynamicPutStrLnImpl terminal
+      }
+
+dynamicGetLine :: (e <: es) => DynamicTerminal e -> Eff es String
+dynamicGetLine terminal = dynamicGetLineImpl (mapHandle terminal)
+
+dynamicPutStrLn :: (e <: es) => DynamicTerminal e -> String -> Eff es ()
+dynamicPutStrLn terminal = dynamicPutStrLnImpl (mapHandle terminal)
+
+runDynamicTerminal ::
+  forall e1 es r.
+  (e1 <: es) =>
+  IOE e1 ->
+  (forall e. DynamicTerminal e -> Eff (e :& es) r) ->
+  Eff es r
+runDynamicTerminal ioe k =
+  useImplIn
+    k
+    MkDynamicTerminal
+      { dynamicGetLineImpl = effIO ioe Prelude.getLine,
+        dynamicPutStrLnImpl = effIO ioe . Prelude.putStrLn
+      }
+
+dynamicTerminalExample :: IO ()
+dynamicTerminalExample = runEff $ \io -> runDynamicTerminal io dynamicTerminalAction
+
+dynamicTerminalAction :: (e <: es) => DynamicTerminal e -> Eff es ()
+dynamicTerminalAction terminal = do
+  dynamicPutStrLn terminal "What is your name?"
+  name <- dynamicGetLine terminal
+  dynamicPutStrLn terminal ("Hello, " <> name)
