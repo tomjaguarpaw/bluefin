9bd031651d20d328387806f7e9db1165fb444ece
Tom Ellis <tom-git@jaguarpaw.co.uk>
2026-09-25 16:48:29 +0100

    (example) Attenuate IOE to Terminal

--- bluefin-examples/src/Bluefin/Examples/Terminal.hs
+++ bluefin-examples/src/Bluefin/Examples/Terminal.hs
@@ -1,16 +1,30 @@
+{-# LANGUAGE DerivingVia #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE TypeOperators #-}
 
 module Bluefin.Examples.Terminal where
 
 import Bluefin.Compound
-  ( mapHandle,
+  ( Handle,
+    OneWayCoercible (oneWayCoercibleImpl),
+    OneWayCoercibleHandle (..),
+    mapHandle,
+    oneWayCoercibleNewtypeHandle,
     useImplIn,
   )
-import Bluefin.Eff (Eff, type (:&), type (<:))
+import Bluefin.Eff (Eff, runEff, type (:&), type (<:))
 import Bluefin.IO (IOE, effIO)
+import Prelude hiding (getLine, putStrLn)
+import Prelude qualified
 
 newtype Terminal e = MkTerminal (IOE e)
+  deriving (Handle) via OneWayCoercibleHandle Terminal
+
+instance (e <: es) => OneWayCoercible (Terminal e) (Terminal es) where
+  oneWayCoercibleImpl = oneWayCoercibleNewtypeHandle @IOE
+
+getLine :: (e <: es) => Terminal e -> Eff es String
+getLine (MkTerminal ioe) = effIO ioe Prelude.getLine
 
 putStrLn :: (e <: es) => Terminal e -> String -> Eff es ()
 putStrLn (MkTerminal ioe) = effIO ioe . Prelude.putStrLn
@@ -22,3 +36,12 @@ runTerminal ::
   (forall e. Terminal e -> Eff (e :& es) r) ->
   Eff es r
 runTerminal ioe k = useImplIn k (MkTerminal (mapHandle ioe))
+
+staticTerminalExample :: IO ()
+staticTerminalExample = runEff $ \io -> runTerminal io staticTerminalAction
+
+staticTerminalAction :: (e <: es) => Terminal e -> Eff es ()
+staticTerminalAction terminal = do
+  putStrLn terminal "What is your name?"
+  name <- getLine terminal
+  putStrLn terminal ("Hello, " <> name)
